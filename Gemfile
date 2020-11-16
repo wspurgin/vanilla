1baafdc61780f27831cc554c6ce8acf303875572
Will Spurgin <will.spurgin@stitchfix.com>
2020-11-16 16:22:09 -0600

    Add pact and reproduction example

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.6.5'
+ruby '2.7.2'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.2', '>= 6.0.2.1'
@@ -24,15 +24,14 @@ gem 'jbuilder', '~> 2.7'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
-
-gem 'cloudinary'
-
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
+gem 'httparty'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'pact'
 end
 
 group :development do
---
+++ spec/service_providers/producer_service_spec.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+require_relative "pact_helper"
+require 'rails_helper'
+
+require 'httparty'
+
+class ThingServiceClient
+  include HTTParty
+  base_uri 'http://thing-service.com'
+
+  def get_things(thing_ids)
+    JSON.parse(self.class.get('/api/things', { query: { ids: thing_ids } }).body)
+  end
+end
+
+RSpec.describe ThingServiceClient, pact: true do
+  before do
+    ThingServiceClient.base_uri 'localhost:1234'
+  end
+
+  subject { ThingServiceClient.new }
+
+  describe "#get_things" do
+    let(:request_path) { "/api/things" }
+    let(:result) { subject.get_things(thing_ids) }
+    let(:response_code) { 200 }
+    let(:thing_ids) { [12345, 67890] }
+    let(:query_params) { { ids: thing_ids }.to_query }
+
+    before do
+      producer_service.
+        given('things exist').
+        upon_receiving("a request to get data for multiple Things").
+        with(
+          method: :get,
+          path: request_path,
+          query: query_params,
+        ).
+        will_respond_with(
+          status: response_code,
+          body: response_body
+        )
+    end
+
+    context "Skus exist" do
+      let(:response_body) do
+        {
+          "things": [
+            "12345": {
+              "id": 12345,
+              "name": Pact.like("Thing 1"),
+            },
+            "67890": {
+              "id": 67890,
+              "name": Pact.like("Thing 2"),
+            }
+          ]
+        }
+      end
+
+      it "responds with skus" do
+        expect(result).to be_a(Hash)
+      end
+    end
+  end
+end

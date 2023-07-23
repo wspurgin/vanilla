a22d0432787b6b2ace51c7ff3528f01260343b98
Will Spurgin <will.spurgin@stitchfix.com>
2023-07-23 14:20:46 -0500

    Refresh vanilla install of Rails 7

--- spec/models/post_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Post, type: :model do
-  it 'should not have any autoload deprecations' do
-    Post.create(name: 'foo', title: 'bar')
-
-    expect(Post.count).to eq(1)
-  end
-end
